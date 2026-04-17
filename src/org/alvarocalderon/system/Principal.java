ae3b480d544d821b27e8a6ccb8c33e78dabd3b7e
Alvaro Calderon <alvarocalderon@kinal.edu.gt>
2026-04-17 11:26:38 -0600

    000 Primer commit del proyecto

---
+++ src/org/alvarocalderon/system/Principal.java
@@ -0,0 +1,13 @@
+package org.alvarocalderon.system;
+
+/**
+ *
+ * @author Alvaro Calderon
+ */
+public class Principal {
+    //main
+    public static void main(String[] args) {
+        //sout
+        System.out.println("HOla mundo estoy en Kinal");
+    }
+}
